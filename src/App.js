579251562a7f7f8a70a75986c90664fdd8372e30
Paula Sanchez <paula.sanchez1031@gmail.com>
2024-12-05 15:53:17 -0500

    feat(components): add container component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "../src/styles/index.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Container from "./components/Container/Container";
 
 function App() {
   return (
     <>
       <Router>
-        <Header />
-        <Main />
-        <Footer />
+        <Container>
+          <Header />
+          <Main />
+          <Footer />
+        </Container>
       </Router>
     </>
   );
